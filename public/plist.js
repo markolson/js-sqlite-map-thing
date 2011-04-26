6456c67561c74eb85c5b7b1ceb4a249efc0afda5
mark olson <theothermarkolson@gmail.com>
2011-04-25 23:28:19 -0400

    Looks like we've got some malformed data coming out. Not a great thing, BUT, most of the data is right, so it's just a matter of figuring out which jerk of a bit is messing me up.

--- public/plist.js
+++ public/plist.js
@@ -34,6 +34,16 @@ function Parser(data) {
         this.ofs += size;
         return value;
     }
+	this.octet = function(size) { 
+		value = this.bytes(size);
+		value = value.split('')
+		value = $.map(value, function(x) { return x.charCodeAt(0) })
+		console.log(value)
+		while(value.length < size) { 
+			value.push(0)
+		}
+		return value;
+	}
 
 	this.varint = function() {
 		segments = [];
@@ -62,11 +72,11 @@ function Parser(data) {
 }
 
 read_column = function(x, payload) { 
-	if(x==0) { return null }
+	if(x==0) { return '' }
 	if(x==1) { return payload.get_int(1) }
 	if(x==2) { return payload.get_int(2) }
 	if(x==4) { return payload.get_int(4) }
-	if(x==7) { return payload.bytes(8) }
+	if(x==7) { return PACK.Unpack('d', payload.octet(8), 0) }
 	if(x > 11 && x%2==0) { return payload.bytes((x-12)/2) }
 	if(x > 12 && x%2==1) { return payload.bytes((x-13)/2) }
 }
